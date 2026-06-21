84378750c794044d1608b450502ac651901b200e
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-21 15:03:42 +0300

    Update ExamPrep.java

--- src/ExamPrep.java
+++ src/ExamPrep.java
@@ -1,14 +1,15 @@
 public class ExamPrep extends StudyTask {
 
-    String examName;     
-    int studyHoursNeeded; 
+    String examName;
+    int studyHoursNeeded;
 
-     
+
     public ExamPrep(String title, String dueDate, int priority, String examName, int studyHoursNeeded) {
         super(title, dueDate, priority);
         this.examName = examName;
         this.studyHoursNeeded = studyHoursNeeded;
     }
+
 
     
     
