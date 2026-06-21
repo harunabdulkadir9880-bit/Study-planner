1707ce00c7be30f6c592302411bd7a3c0ffc8cbc
MATISAM11 <matias.samson@bitscollege.edu.et>
2026-06-21 21:52:35 +0300

    1

--- src/BinaryFilemanager.java
+++ src/BinaryFilemanager.java
@@ -1,2 +1,14 @@
-public class BinaryFilemanager {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+public class BinaryFilemanager {public static void saveAllTasks(ArrayList<StudyTask> list) {
+        try {
+            DataOutputStream dos = new DataOutputStream(new FileOutputStream(FileHelper.binaryFile, false));
+
+            for (int i = 0; i < list.size(); i++) {
+                StudyTask task = list.get(i);
 }
