86ee57a6fabb4ad9ee91a8ae7c2291d131c7189c
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-21 13:50:48 +0300

    3 methods to mark task completed,show details,get tsak type

--- src/StudyTask.java
+++ src/StudyTask.java
@@ -12,4 +12,18 @@ public class StudyTask  implements Serializable {
         this.completed = false; // tasks start as not completed
     }
 
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    public void showDetails() {
+        System.out.println("Title    : " + title);
+        System.out.println("Due Date : " + dueDate);
+        System.out.println("Priority : " + priority);
+        System.out.println("Completed: " + completed);
+    }
+    public String getTaskType() {
+        return "StudyTask";
+    }
+
 }
