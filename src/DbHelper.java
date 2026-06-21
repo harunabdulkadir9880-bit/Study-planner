d3970ac5a32afb5df9bcbf0601e1a508fd34dddd
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-21 22:07:28 +0300

    5 values to match my own Mysql workbench setup and full JDBC url built from those values

--- src/DbHelper.java
+++ src/DbHelper.java
@@ -1,2 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbHelper {
+
+    public static String host     = "localhost";
+    public static String port     = "3306";
+    public static String dbName   = "study_planner_db";
+    public static String username = "root";
+    public static String password = "password";
+
+    public static String dbUrl = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
+
+
 }
