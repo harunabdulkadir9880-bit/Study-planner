0f4e913d0901c9fc17e010299e1e3b634b08e0df
abebawkaleb08-sys <abebawkaleb08@gmail.com>
2026-06-21 13:51:01 +0200

    - Create ExamPrep class as a subclass of StudyTask - Add specific fields for examName and studyHoursNeeded - Implement a constructor that invokes the superclass constructor using super()

--- src/ExamPrep.java
+++ src/ExamPrep.java
@@ -1,2 +1,14 @@
-public class ExamPrep {
-}
+public class ExamPrep extends StudyTask {
+
+    String examName;     
+    int studyHoursNeeded; 
+
+     
+    public ExamPrep(String title, String dueDate, int priority, String examName, int studyHoursNeeded) {
+        super(title, dueDate, priority);
+        this.examName = examName;
+        this.studyHoursNeeded = studyHoursNeeded;
+    }
+
+    
+    
