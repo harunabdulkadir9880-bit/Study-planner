4e42a31c3d98d194cf214c142b92d03e52115ba7
MATISAM11 <matias.samson@bitscollege.edu.et>
2026-06-21 21:57:43 +0300

    2

--- src/BinaryFilemanager.java
+++ src/BinaryFilemanager.java
@@ -5,10 +5,41 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 
-public class BinaryFilemanager {public static void saveAllTasks(ArrayList<StudyTask> list) {
+public class BinaryFileManager {
+
+    
+    public static void saveAllTasks(ArrayList<StudyTask> list) {
         try {
             DataOutputStream dos = new DataOutputStream(new FileOutputStream(FileHelper.binaryFile, false));
 
             for (int i = 0; i < list.size(); i++) {
                 StudyTask task = list.get(i);
-}
+
+                
+                dos.writeUTF(task.getTaskType());
+                dos.writeUTF(task.title);
+                dos.writeUTF(task.dueDate);
+                dos.writeInt(task.priority);
+                dos.writeBoolean(task.completed);
+
+                
+                if (task instanceof DailyTask) {
+                    DailyTask d = (DailyTask) task;
+                    dos.writeUTF(d.subject);
+
+                } else if (task instanceof ExamPrep) {
+                    ExamPrep e = (ExamPrep) task;
+                    dos.writeUTF(e.examName);
+                    dos.writeInt(e.studyHoursNeeded);
+
+                } else if (task instanceof ProjectTask) {
+                    ProjectTask p = (ProjectTask) task;
+                    dos.writeUTF(p.groupMembers);
+                    dos.writeInt(p.percentComplete);
+                }
+            }
+
+            dos.close();
+           
+
+
