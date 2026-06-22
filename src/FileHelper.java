0ecba6851579e5ac9fa85402a7f80ef6362c4557
abebawkaleb08-sys <abebawkaleb08@gmail.com>
2026-06-22 21:20:32 +0200

    Add FileHelper utility for task file management  Initialize file path constants for data storage  Set up FileHelper with text, binary, and object file paths

--- src/FileHelper.java
+++ src/FileHelper.java
@@ -1,2 +1,10 @@
+import java.io.File;
+import java.util.Date;
+
 public class FileHelper {
-}
+
+    public static String dataFolder = "data";
+    public static String textFile   = "data/tasks.txt";
+    public static String binaryFile = "data/tasks.dat";
+    public static String objectFile = "data/tasks.obj";
+
