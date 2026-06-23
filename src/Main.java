bbd9fcda1a6d3015a4fde9e501b12f9d7a8bbcc1
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-23 08:55:03 +0300

    Set up folders/files and try to set up the database too

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
+        FileHelper.setupFiles();
+        DbHelper.setupDatabase();
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+
+        System.out.println("\nWelcome to the Personal Study Planner!");
+
 
     }
 }
