2284863e268bca2f2b7197fa028b8ee5fda1152b
Harun Abdulkadir <harunabdulkadir9880@gmail.com>
2026-06-23 08:47:40 +0300

    Fixed many errors within the Filehelper class

--- src/FileHelper.java
+++ src/FileHelper.java
@@ -9,47 +9,47 @@ public class FileHelper {
     public static String binaryFile = "data/tasks.dat";
     public static String objectFile = "data/tasks.obj";
 
-public static void setupFiles() {
-    try {
-        File data = new File(dataFolder);
-        if (!data.exists()) {
-            data.mkdir();
-            System.out.println("Created folder: " + dataFolder);
-        }
-        File txt = new File(textFile);
-        if (!txt.exists()) {
-            txt.createNewFile();
-            System.out.println("Created file: " + textFile);
-        }
-        File dat = new File(binaryFile);
-        if (!dat.exists()) {
-            dat.createNewFile();
-            System.out.println("Created file: " + binaryFile);
-        }
-      catch(Exception e){
+    public static void setupFiles() {
+        try {
+            File data = new File(dataFolder);
+            if (!data.exists()) {
+                data.mkdir();
+                System.out.println("Created folder: " + dataFolder);
+            }
+
+            File txt = new File(textFile);
+            if (!txt.exists()) {
+                txt.createNewFile();
+                System.out.println("Created file: " + textFile);
+            }
+
+            File dat = new File(binaryFile);
+            if (!dat.exists()) {
+                dat.createNewFile();
+                System.out.println("Created file: " + binaryFile);
+            }
+
+        } catch (Exception e) {
             System.out.println("Error setting up files: " + e.getMessage());
         }
     }
 
-public static void showFileProperties(String filePath) {
-    File file = new File(filePath);
-
-    System.out.println("\n--- File Properties: " + filePath + " ---");
-}
     public static void showFileProperties(String filePath) {
-    File file = new File(filePath);
+        File file = new File(filePath);
 
-    System.out.println("\n--- File Properties: " + filePath + " ---");
+        System.out.println("\n--- File Properties: " + filePath + " ---");
 
-    if (!file.exists()) {
-        System.out.println("File does not exist.");
-        return;
+        if (!file.exists()) {
+            System.out.println("File does not exist.");
+            return;
+        }
+
+        System.out.println("Name          : " + file.getName());
+        System.out.println("Absolute Path : " + file.getAbsolutePath());
+        System.out.println("Size (bytes)  : " + file.length());
+        System.out.println("Last Modified : " + new Date(file.lastModified()));
+        System.out.println("------------------------------------------");
     }
+}
 
-    System.out.println("Name          : " + file.getName());
-    System.out.println("Absolute Path : " + file.getAbsolutePath());
-    System.out.println("Size (bytes)  : " + file.length());
-    System.out.println("Last Modified : " + new Date(file.lastModified()));
-    System.out.println("------------------------------------");
- } 
-}    
+
